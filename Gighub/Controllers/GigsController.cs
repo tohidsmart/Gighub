0f53b94401ea6e3c0fedf6befb2ece9d856cf5bd
tohidsmart <tohidsmart@gmail.com>
2016-06-05 14:54:06 +0800

    Add New Form
    Added New From for creating New Gig

---
+++ Gighub/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using Gighub.Models;
+using Gighub.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Gighub.Controllers
+{
+	public class GigsController : Controller
+	{
+		private ApplicationDbContext _context;
+
+		public GigsController()
+		{
+			_context=new ApplicationDbContext();
+		}
+		
+		public ActionResult Create()
+		{
+			var model = new GigFormViewModel
+			{
+				Genres = _context.Genres.ToList()
+			};
+			
+			return View(model);
+		}
+
+
+	}
+}
