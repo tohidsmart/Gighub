1f5fc98ed6b554249659786ac9fc3f5a8ba61f35
Tohid Azimi <tohidsmart@gmail.com>
2016-06-09 19:35:10 +0800

    Added HomePage Markup & Style

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -13,27 +13,30 @@ namespace Gighub.Controllers
 
         public HomeController()
         {
-                this._context=new ApplicationDbContext();
+            _context = new ApplicationDbContext();
         }
-		public ActionResult Index()
-		{
-		    var upcomingGigs = _context.Gigs.Include(g => g.Artist).Where(g => g.DateTime > DateTime.Now);
-			return View(upcomingGigs);
-		}
+        public ActionResult Index()
+        {
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+               .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingGigs);
+        }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
-			return View();
+            return View();
 
-		}
-	}
+        }
+    }
 }
